96d194d9eae901758c673081354e61ec8f441f57
Dongchuel Kim <terrinens@gmail.com>
2023-08-04 15:05:24 +0900

    fix : 로그인 오류 수정 시큐리티 추가로 인한 로그인 스택오버 플로우 수정
    feat : 권한부여 구현
    MemberRole에 따라서 멤버의 권한 부여 구현

--- src/main/java/com/cloth/clothshop/Member/MemberRepository.java
+++ src/main/java/com/cloth/clothshop/Member/MemberRepository.java
@@ -2,6 +2,9 @@ package com.cloth.clothshop.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, String> {
 
+    Optional<Member> findMemberById (String id);
 }
