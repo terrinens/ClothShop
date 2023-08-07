3af79aa0a66b99ad1e7fbf820d5e320538b28c36
Dongchuel Kim <terrinens@gmail.com>
2023-08-07 17:06:28 +0900

    fix : 상품 이동 버튼 수정 상품 이동후, 또 한번더 다른 상품으로 이동시 링크가 중첩되던 오류를 수정함.

--- src/main/java/com/cloth/clothshop/Products/ProductsController.java
+++ src/main/java/com/cloth/clothshop/Products/ProductsController.java
@@ -16,14 +16,19 @@ public class ProductsController {
     ProductsService pService;
 
     @GetMapping("/cloth/sleeve")
-    public String sleeve() {
+    public String sleeveList(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+
+        char kindOption1 = 'a'; char kindOption2 = 'b';
+
+        Page<Products> paging = pService.getlistCloth(kindOption1, kindOption2, page);
+
+        model.addAttribute("paging", paging);
 
         return "cloths/sleeve";
     }
 
     @GetMapping("/cloth/pants")
-    public String pantsList(Model model, @RequestParam(value = "page", defaultValue = "0") int page
-    ) {
+    public String pantsList(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
 
         char kindOption1 = 'c'; char kindOption2 = 'd';
 
@@ -35,25 +40,14 @@ public class ProductsController {
     }
 
     @GetMapping("/cloth/skirt")
-    public String skirt() {
+    public String skirt(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
+
+        char kindOption1 = 'e'; char kindOption2 = 'f';
+
+        Page<Products> paging = pService.getlistCloth(kindOption1, kindOption2, page);
+
+        model.addAttribute("paging", paging);
 
         return "cloths/skirt";
     }
-
-/*    @GetMapping("/list")
-    public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page,
-                       @RequestParam(value = "kw", defaultValue = "") String kw) {
-
-        //2. 비즈니스 로직처리
-        //List<Question> questionList = this.questionRepostiroy.findAll();
-        Page<Question> paging = questionService.getList(page, kw);
-
-        //3. 받아온 List를 client로 전송(Model 객체에 저장해서 Client로 전송)
-        model.addAttribute("paging", paging);
-
-        paging.getTotalPages();
-
-        return "question_list";
-    }*/
-
 }
