7448df8e5c61b2343bc6b7cbe3b3e53d967de702
Dongchuel Kim <terrinens@gmail.com>
2023-08-07 18:18:33 +0900

    feat : 매니지먼트에 필요한 매핑 추가 nav.html 링크 추가 및 MainController 맵핑추가

--- src/main/java/com/cloth/clothshop/Controller/MainController.java
+++ src/main/java/com/cloth/clothshop/Controller/MainController.java
@@ -11,9 +11,27 @@ public class MainController {
 		return "index";
 	}
 
+	@GetMapping("/management/allitem")
+	public String managementAllitem() {
+
+		return "management/allitem_management";
+	}
+
 	@GetMapping("/management/member")
 	public String managementMember() {
 
 		return "management/member_management";
+	}
+
+	@GetMapping("/management/order")
+	public String managementOrder() {
+
+		return "management/order_management";
+	}
+
+	@GetMapping("/management/recommended")
+	public String managementRecommended() {
+
+		return "management/recommended_management";
 	}
 }
