a5eb6169e232c1113a52ebe83f5df12e6e52e107
Dongchuel Kim <terrinens@gmail.com>
2023-08-11 17:55:58 +0900

    fix : 매니지먼트 컨트롤 수정 매니지먼트 : 멤버 리스트를 불러올때 @@RequestParam 대신 HttpServletRequest로 받게끔 변경.

--- src/main/java/com/cloth/clothshop/Management/ManagementController.java
+++ src/main/java/com/cloth/clothshop/Management/ManagementController.java
@@ -2,6 +2,7 @@ package com.cloth.clothshop.Management;
 
 import com.cloth.clothshop.Member.Member;
 import com.cloth.clothshop.Member.MemberService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -18,16 +19,25 @@ public class ManagementController {
     private final MemberService mService;
 
     @GetMapping("/allitem")
-    public String managementAllitem() {
+    public String managementAllitem(Model model, ManagementNewItemForm mnewItemForm, HttpServletRequest request) {
 
         return "management/allitem_management";
     }
 
     @GetMapping("/member")
-    public String managementMember(Model model, ManagementMemberForm mmForm,
-                                   @RequestParam(value = "page", defaultValue = "0") int page,
-                                   @RequestParam(value = "option", defaultValue = "") String option,
-                                   @RequestParam(value = "keyword", defaultValue = "") String keyword) {
+    public String managementMember(Model model, ManagementMemberForm mmForm, HttpServletRequest request) {
+
+        int page;
+        if (request.getParameter("page") == null) {
+
+            page = 0;
+        } else {
+
+            page = Integer.parseInt(request.getParameter("page"));
+        }
+
+        String option = request.getParameter("option");
+        String keyword = request.getParameter("keyword");
 
         Page<Member> paging = mService.managementGetMemberList(page, option, keyword);
 
