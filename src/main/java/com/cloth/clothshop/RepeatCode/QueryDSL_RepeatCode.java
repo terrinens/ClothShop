d41d3c72c6c83982508931b0e14363fe1b8ff19b
Dongchuel <terrinens@gamil.com>
2023-08-22 01:08:37 +0900

    fix : total 계산값 버그 수정

--- src/main/java/com/cloth/clothshop/RepeatCode/QueryDSL_RepeatCode.java
+++ src/main/java/com/cloth/clothshop/RepeatCode/QueryDSL_RepeatCode.java
@@ -20,20 +20,32 @@ public class QueryDSL_RepeatCode {
 
         JPAQuery<T> query = queryFactory.selectFrom(table);
         long total = query.fetch().size();
+        System.out.println("jpa 리펙트코드 테이블 :::: " + table);
         List<T> page;
         if (condition != null && !searchKeyword.trim().isEmpty()) {
+            total = query
+                    .where(condition)
+                    .fetch().size();
 
             page = query
                     .where(condition)
                     .offset(pageable.getOffset())
                     .limit(pageable.getPageSize())
                     .fetch();
+
+            System.out.println("jpa 리펙트코드 Listpage 저장 비어있지 않음 사이즈 :::: " + page.size());
+            System.out.println("jpa 리펙트코드 Listpage 저장 비어있지 않음 0번 저장값 :::: " + page.toString());
+            System.out.println("jpa 리펙트코드 total 값 :::: " + total);
         } else {
 
             page = query
                     .offset(pageable.getOffset())
                     .limit(pageable.getPageSize())
                     .fetch();
+
+            System.out.println("jpa 리펙트코드 Listpage  저장 비어있음 저장 사이즈 :::: " + page.size());
+            System.out.println("jpa 리펙트코드 Listpage  저장 비어있음 0번 저장값 :::: " + page.toString());
+            System.out.println("jpa 리펙트코드 total 값 :::: " + total);
         }
 
         return new PageImpl<>(page, pageable, total);
