718df6cb88cddd0c41e7663868b8fbbcd6301a05
Dongchuel Kim <terrinens@gmail.com>
2023-08-25 17:13:04 +0900

    fix : 잘못된 대입 수정
    Signed-off-by: Dongchuel Kim <terrinens@gmail.com>

--- src/main/resources/static/managementJS/TimestampConversionDate.js
+++ src/main/resources/static/managementJS/TimestampConversionDate.js
@@ -2,7 +2,7 @@ function conversionDate(timestamp) {
     if (timestamp == null) {
         return "";
     } else {
-        let date = new Date(date);
+        let date = new Date(timestamp);
         let year = date.getFullYear();
         let month = (date.getMonth() + 1).toString().padStart(2, '0');
         let day = date.getDate().toString().padStart(2, '0');
