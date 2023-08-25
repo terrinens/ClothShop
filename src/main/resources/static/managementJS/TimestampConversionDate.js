b922dc02c3e1d113c4c983f5957beb72629fc711
Dongchuel Kim <terrinens@gmail.com>
2023-08-25 17:12:34 +0900

    refactor : 파일 이름 리펙토링
    Signed-off-by: Dongchuel Kim <terrinens@gmail.com>

--- src/main/resources/static/managementJS/TimestampConversionDate.js
+++ src/main/resources/static/managementJS/TimestampConversionDate.js
@@ -1,8 +1,8 @@
-function conversionDate(indate) {
-    if (indate == null) {
+function conversionDate(timestamp) {
+    if (timestamp == null) {
         return "";
     } else {
-        let date = new Date(indate);
+        let date = new Date(date);
         let year = date.getFullYear();
         let month = (date.getMonth() + 1).toString().padStart(2, '0');
         let day = date.getDate().toString().padStart(2, '0');
