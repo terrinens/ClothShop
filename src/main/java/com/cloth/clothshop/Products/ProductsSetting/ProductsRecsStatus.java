769dc186d8a26af7a55ee0537b42cd8db4cb0b6a
terrinens <terrinens@mgmail.com>
2023-09-03 16:09:06 +0900

    feat : html 상품 추천 상태 수정 기능 추가

--- src/main/java/com/cloth/clothshop/Products/ProductsSetting/ProductsRecsStatus.java
+++ src/main/java/com/cloth/clothshop/Products/ProductsSetting/ProductsRecsStatus.java
@@ -3,7 +3,7 @@ package com.cloth.clothshop.Products.ProductsSetting;
 import lombok.Getter;
 
 public enum ProductsRecsStatus {
-    notRecommend(0), Recommend(1);
+    no(0), yes(1);
     private final int status;
 
     ProductsRecsStatus(int status) {
@@ -12,13 +12,13 @@ public enum ProductsRecsStatus {
 
     public static ProductsRecsStatus fromStatus(int status) {
         return switch (status) {
-            case 0 -> notRecommend;
-            case 1 -> Recommend;
+            case 0 -> no;
+            case 1 -> yes;
             default -> throw new IllegalStateException("Unexpected value: " + status);
         };
     }
 
     public int getStatus() {
-        return status;
+        return this.status;
     }
 }
